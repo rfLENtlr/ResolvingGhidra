e5636dc3a63178cf9273c8c6f8080db32aea12e0
rfLENtlr <gandamyuya@gmail.com>
2024-10-20 22:03:00 +0900

    fix: update script.bat, username independent

--- script.bat
+++ script.bat
@@ -1,18 +1,14 @@
-
-
-REM this is an example of GhidraHeadlessScript
 @echo off
-REM Path to Ghidra's headless analyzer script
+set PROJECT_DIR=%CD%
 set GHIDRA_PATH=C:\Tools\ghidra_11.1.2_PUBLIC\support\analyzeHeadless.bat
 
 REM Arguments for Ghidra headless execution
-set PROJECT_DIR=C:\Users\joker
 set REPO_NAME=ghidra_repo
-set EXECUTABLE=API_hashing_2.exe
+set EXECUTABLE=%PROJECT_DIR%\samples\API_hashing_2.exe
 set SCRIPT_PATH=ghidra_scripts
 set SCRIPT_NAME=emulate_by_step_observation.java
 
 REM Execute Ghidra headless analysis with the specified script
-%GHIDRA_PATH% %PROJECT_DIR% %REPO_NAME% -process %EXECUTABLE% -scriptPath %SCRIPT_PATH% -postScript %SCRIPT_NAME%
+%GHIDRA_PATH% %PROJECT_DIR% %REPO_NAME% -import %EXECUTABLE% -scriptPath %SCRIPT_PATH% -postScript %SCRIPT_NAME%
 
 pause
